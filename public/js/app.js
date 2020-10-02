fca1ab413b5e918e9ccffc17e5ce0c78cdfb8220
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-02 17:29:02 +0100

    add import sessions

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,19 @@
-(function() {
+import getSessions from './sessionRepository.js';
 
 function render() {
-  var list = document.querySelector('#sessions');
+  var list = document.querySelector("#sessions");
   if (!list) return;
   list.innerHTML = sessionTemplate(data.listItems);
-};
+}
 
 
 var data = {
-  listItems: []
+  listItems: [],
 };
 
 getSessions()
-  .then((sessions)=>{
-    console.log('promises!')
-    data.listItems = sessions;
-    render();
-  });
-}())
+.then((sessions) => {
+  console.log("promises!");
+  data.listItems = sessions;
+  render();
+});
